445236b501887c9c24bb6938b6fc06e9c3990fb6
lizhenyang <lizhenyang@jd.com>
2018-11-21 14:06:27 +0800

    use example codes for testing

--- src/gtest/test_cslzy_sin_layer.cpp
+++ src/gtest/test_cslzy_sin_layer.cpp
@@ -26,5 +26,56 @@ namespace caffe {
             blob_top_vec_.push_back(blob_top_);
         }
         virtual ~CslzySinLayerTest() { delete blob_bottom_; delete blob_top_; }
-    }
+
+        void TestForward(Dtype filler_std)
+        {
+            FillerParameter filler_param;
+            filler_param.set_std(filler_std);
+            GaussianFiller<Dtype> filler(filler_param);
+            filler.Fill(this->blob_bottom_);
+
+            LayerParameter layer_param;
+            CslzySinLayer<Dtype> layer(layer_param);
+            layer.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+            layer.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+            // Now, check values
+            const Dtype* bottom_data = this->blob_bottom_->cpu_data();
+            const Dtype* top_data = this->blob_top_->cpu_data();
+            const Dtype min_precision = 1e-5;
+            for (int i = 0; i < this->blob_bottom_->count(); ++i) {
+              Dtype expected_value = sin(bottom_data[i]);
+              Dtype precision = std::max(
+                Dtype(std::abs(expected_value * Dtype(1e-4))), min_precision);
+              EXPECT_NEAR(expected_value, top_data[i], precision);
+            }
+          }
+
+          void TestBackward(Dtype filler_std)
+          {
+              FillerParameter filler_param;
+              filler_param.set_std(filler_std);
+              GaussianFiller<Dtype> filler(filler_param);
+              filler.Fill(this->blob_bottom_);
+
+              LayerParameter layer_param;
+              CslzySinLayer<Dtype> layer(layer_param);
+              GradientChecker<Dtype> checker(1e-4, 1e-2, 1701);
+              checker.CheckGradientEltwise(&layer, this->blob_bottom_vec_,
+                  this->blob_top_vec_);
+            }
+
+         Blob<Dtype>* const blob_bottom_;
+         Blob<Dtype>* const blob_top_;
+         vector<Blob<Dtype>*> blob_bottom_vec_;
+         vector<Blob<Dtype>*> blob_top_vec_;
+        };
+        TYPED_TEST_CASE(SinLayerTest, TestDtypesAndDevices);
+        TYPED_TEST(SinLayerTest, TestSin) {
+          this->TestForward(1.0);
+        }
+        TYPED_TEST(SinLayerTest, TestSinGradient) {
+          this->TestBackward(1.0);
+        }
+
+
 } // namespace caffe
