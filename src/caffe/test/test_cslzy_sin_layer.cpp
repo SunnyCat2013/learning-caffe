e48b02114b4482d2fe20aa593c36557df82e871d
lizhenyang <lizhenyang@jd.com>
2018-11-21 21:38:04 +0800

    FilterParameter to FillerParameter

--- src/caffe/test/test_cslzy_sin_layer.cpp
+++ src/caffe/test/test_cslzy_sin_layer.cpp
@@ -6,11 +6,11 @@
 #include "caffe/blob.hpp"
 #include "caffe/common.hpp"
 #include "caffe/filler.hpp"
+#include "caffe/layers/cslzy_sin_layer.hpp"
 
 #include "caffe/test/test_caffe_main.hpp"
 #include "caffe/test/test_gradient_check_util.hpp"
 
-#include "caffe/layers/cslzy_sin_layer.hpp"
 
 namespace caffe {
     template <typename TypeParam>
@@ -22,7 +22,7 @@ namespace caffe {
               blob_top_(new Blob<Dtype>())
         {
             Caffe::set_random_seed(2018);
-            FilterParameter filler_param;
+            FillerParameter filler_param;
             blob_bottom_vec_.push_back(blob_bottom_);
             blob_top_vec_.push_back(blob_top_);
         }
